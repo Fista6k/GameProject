e280763cbd4698f844346163cb93e90e82802dcd
enjxyer <hfgtylop@gmail.com>
2025-05-13 16:42:18 +0500

    Complete1mini-game

--- Assets/Scripts/ChooseMenu.cs
+++ Assets/Scripts/ChooseMenu.cs
@@ -6,13 +6,31 @@ public class PopupManager : MonoBehaviour
     [SerializeField] private Image darkBackground;
     [SerializeField] private GameObject popupWindow;
     [SerializeField] private float fadeDuration = 0.6f;
+    [SerializeField] private Button openButton;
+    [SerializeField] private Button closeButton;
 
     private void Start()
     {
-        darkBackground.color = new Color(0, 0, 0, 0);
-        darkBackground.raycastTarget = false;
+        FadeBackground(fadeDuration);
+        popupWindow.SetActive(true);
+        closeButton.gameObject.SetActive(true);
+        openButton.gameObject.SetActive(false);
+    }
+
+    public void OpenClick()
+    {
+        openButton.gameObject.SetActive(false);
+        closeButton.gameObject.SetActive(true);
         popupWindow.SetActive(true);
         FadeBackground(fadeDuration);
+    }
+
+    public void CloseClick()
+    {
+        closeButton.gameObject.SetActive(false);
+        openButton.gameObject.SetActive(true);
+        popupWindow.SetActive(false);
+        darkBackground.gameObject.SetActive(false);
     }
 
     private void FadeBackground(float targetAlpha)
